5c4a808f57aaf9f0fe52c3ca299b5d8d47b79614
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-04 19:41:15 +0700

    feat: App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>home</div>,
+  },
+  {
+    path: '/menu',
+    element: <div>menu</div>,
+  },
+  {
+    path: '/cart',
+    element: <div>cart</div>,
+  },
+  {
+    path: '/order/new',
+    element: <div>new order</div>,
+  },
+  {
+    path: '/order/:orderId',
+    element: <div>order</div>,
+  },
+]);
+
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
