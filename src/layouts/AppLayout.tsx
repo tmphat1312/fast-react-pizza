75056f89aec11f5f49e2dfe8a1795ed01120183e
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-05 11:47:08 +0700

    feat: Add loading indicator

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,17 @@
 import { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import Footer from './Footer';
 import Header from './Header';
+import LoadingIndicator from '../ui/LoadingIndicator';
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+
   return (
     <Fragment>
+      {navigation.state == 'loading' && <LoadingIndicator />}
+
       <Header />
 
       <main>
