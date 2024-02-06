cfdfffeabf18ab9bea836a9ddb18bf79ecf3941e
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-06 21:21:02 +0700

    style: Add button sizes and variants

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,14 +1,34 @@
 import { twMerge } from 'tailwind-merge';
 
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: 'sm' | 'md';
+  variant?: 'primary';
+};
+
+const SIZE_VARIANTS = {
+  sm: 'px-3  py-2 text-xs md:px-4 md:py-2.5',
+  md: 'px-4 py-3 text-base md:px-5',
+};
+
+const STYLE_VARIANTS = {
+  primary: 'bg-primary-400 text-stone-800',
+};
+
 export default function Button({
   children,
+  size,
+  variant,
   className,
   ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+}: ButtonProps) {
   return (
     <button
       className={twMerge(
-        'inline-block px-3 py-2 md:px-4 md:py-3 font-semibold tracking-wide uppercase transition-colors rounded-full bg-primary-400 text-stone-800 hover:bg-primary-300 active:bg-primary-500 disabled:cursor-not-allowed',
+        'inline-block font-semibold tracking-wide uppercase  ',
+        '[&:not(:disabled)]:hover:brightness-110 [&:not(:disabled)]:active:brightness-90',
+        'transition-colors rounded-full disabled:grayscale disabled:cursor-not-allowed',
+        SIZE_VARIANTS[size ?? 'md'],
+        STYLE_VARIANTS[variant ?? 'primary'],
         className,
       )}
       {...props}
