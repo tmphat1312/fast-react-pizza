33aeb2486acaaa5f5ad5e9e614ad24d46f276f7e
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-06 20:45:17 +0700

    config: Progress enhancement for h-screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        screen: ['100vh', '100dvh'],
+      },
       colors: {
         primary: colors.yellow,
       },
