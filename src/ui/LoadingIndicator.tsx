8d9f8735bdeef0e4d25c593ded109cc5aaefb346
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-06 20:35:49 +0700

    style: Position loading indicator

--- src/ui/LoadingIndicator.tsx
+++ src/ui/LoadingIndicator.tsx
@@ -1,5 +1,9 @@
 import styles from './LoadingIndicator.module.css';
 
 export default function LoadingIndicator() {
-  return <div className={styles.loader} aria-label="loading" role="alert" />;
+  return (
+    <div className="absolute inset-0 grid bg-slate-200/20 backdrop-blur-sm place-content-center">
+      <div className={styles.loader} aria-label="loading" role="alert" />
+    </div>
+  );
 }
