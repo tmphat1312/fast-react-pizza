6f375df7011ff896e71055c2271dff5eef82039b
Truong Minh Phat <tmphat1312@gmail.com>
2024-03-08 20:51:52 +0700

    style: add container to header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,7 +5,7 @@ import Username from '../ui/Username';
 export default function Header() {
   return (
     <header className="px-4 py-3 uppercase border-b bg-primary-400 border-stone-200 sm:px-6">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between max-w-4xl mx-auto">
         <Link to="/" className="tracking-widest">
           Fast React Pizza Co.
         </Link>
